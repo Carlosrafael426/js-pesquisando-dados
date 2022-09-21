14f088ab2739b43140030147d7bbaca7a9acb48f
yancarlodev <yancarlodc@gmail.com>
2022-09-21 17:23:12 -0300

    refactor: change style and HTML structure

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+
+form.addEventListener('submit', procuraAlimento)
+
+function procuraAlimento(event) {
+    event.preventDefault()
+    const valorDoInput = input.value
+
+    /* Desenvolva sua a partir daqui lógica aqui */
+    
+}
+
